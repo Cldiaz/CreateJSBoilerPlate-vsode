43227df9a73dc7b0aeca7bea63e8b3381802c6b2
Cindy Diaz <cindy.liliana.diaz@hotmail.com>
2016-05-30 13:33:18 -0400

    Added tsd suppeort for jquery

--- Scripts/core/_reference.js
+++ Scripts/core/_reference.js
@@ -1,3 +1,4 @@
+/// <reference path="../../typings/tsd.d.ts" />
 /// <reference path="../objects/person.ts" />
-/// <reference path="../objects/student.ts" /> 
+/// <reference path="../objects/student.ts" />
 //# sourceMappingURL=_reference.js.map
